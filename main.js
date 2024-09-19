0c073280bf8a0f66fe2f4bdf41380e1a052ceb47
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2024-09-19 23:57:56 +0530

    Added a function to randomly generate rps

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("check");
+// console.log("check");
+
+function getComputerChoice() {
+  let choice = ["rock", "paper", "scissor"];
+  let num = Math.floor((Math.random() * 10) % 3);
+
+  return choice[num];
+}
+
+console.log(getComputerChoice());
