b6d7761a2e18c408a469c6152525bb5290dc62c7
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2024-09-20 00:22:09 +0530

    Added logic for a single round, Also added a score counter

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // console.log("check");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissor"];
@@ -12,5 +14,46 @@ function getHumanChoice() {
   return ch;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  humanChoice =
+    humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+  console.log("Human chose: " + humanChoice);
+  console.log("Computer chose: " + computerChoice);
+
+  if (humanChoice === computerChoice) {
+    console.log(`Tie!! Both chose ${humanChoice}`);
+  } else if (humanChoice === "Paper") {
+    if (computerChoice === "Rock") {
+      console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lost! ${humanChoice} loses to ${computerChoice}`);
+      computerScore++;
+    }
+  } else if (humanChoice === "Rock") {
+    if (computerChoice === "Scissor") {
+      console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lost! ${humanChoice} loses to ${computerChoice}`);
+      computerScore++;
+    }
+  } else {
+    if (computerChoice === "Paper") {
+      console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lost! ${humanChoice} loses to ${computerChoice}`);
+      computerScore++;
+    }
+  }
+
+  console.log(`Human Score: ${humanScore}`);
+  console.log(`Computer Score: ${computerScore}`);
+}
+
+compCh = getComputerChoice();
+humanCh = getHumanChoice();
+
+playRound(humanCh, compCh);
