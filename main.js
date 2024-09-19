6936c7092354447d766f29a2ea30e2cfbefda767
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2024-09-20 00:02:38 +0530

    added function to input human choice

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // console.log("check");
 
 function getComputerChoice() {
-  let choice = ["rock", "paper", "scissor"];
+  let choice = ["Rock", "Paper", "Scissor"];
   let num = Math.floor((Math.random() * 10) % 3);
 
   return choice[num];
 }
 
+function getHumanChoice() {
+  let ch = prompt("Pick :- Rock, Paper, Scissor?");
+  return ch;
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
