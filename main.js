6d871bd51e0e8fffe88cbe19a0ab9917b95b6f52
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2024-09-24 16:42:11 +0530

    Some changes to JS and added styles

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-// console.log("check");
+const msg = document.querySelector(".msg");
+const playerCh = document.querySelector(".player-ch");
+const computerCh = document.querySelector(".computer-ch");
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -6,60 +9,85 @@ function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissor"];
   let num = Math.floor((Math.random() * 10) % 3);
 
-  return choice[num];
-}
+  computerCh.textContent = `Computer : ${choice[num]}`;
 
-function getHumanChoice() {
-  let ch = prompt("Pick :- Rock, Paper, Scissor?");
-  return ch;
+  return choice[num];
 }
 
 function playRound(humanChoice, computerChoice) {
   humanChoice =
     humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
 
-  //   console.log("Human chose: " + humanChoice);
-  //   console.log("Computer chose: " + computerChoice);
-
   if (humanChoice === computerChoice) {
-    console.log(`Tie!! Both chose ${humanChoice}`);
+    msg.textContent = `Tie!! Both chose ${humanChoice}`;
   } else if (humanChoice === "Paper") {
     if (computerChoice === "Rock") {
-      console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+      msg.textContent = `You Won! ${humanChoice} beats ${computerChoice}`;
       humanScore++;
     } else {
-      console.log(`You lost! ${humanChoice} loses to ${computerChoice}`);
+      msg.textContent = `You lost! ${humanChoice} loses to ${computerChoice}`;
       computerScore++;
     }
   } else if (humanChoice === "Rock") {
     if (computerChoice === "Scissor") {
-      console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+      msg.textContent = `You Won! ${humanChoice} beats ${computerChoice}`;
       humanScore++;
     } else {
-      console.log(`You lost! ${humanChoice} loses to ${computerChoice}`);
+      msg.textContent = `You lost! ${humanChoice} loses to ${computerChoice}`;
       computerScore++;
     }
   } else {
     if (computerChoice === "Paper") {
-      console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
+      msg.textContent = `You Won! ${humanChoice} beats ${computerChoice}`;
       humanScore++;
     } else {
-      console.log(`You lost! ${humanChoice} loses to ${computerChoice}`);
+      msg.textContent = `You lost! ${humanChoice} loses to ${computerChoice}`;
       computerScore++;
     }
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
+const allBtn = document.querySelector(".all-btn");
+
+allBtn.addEventListener("click", (event) => {
+  let target = event.target;
+  switch (target.className) {
+    case "rock-btn":
+      playerCh.textContent = "Player: Rock ";
+      playGame("rock");
+      break;
+    case "paper-btn":
+      playerCh.textContent = `Player: Paper`;
+      playGame("paper");
+      break;
+    case "scissor-btn":
+      playerCh.textContent = "Player: Scissor";
+      playGame("scissor");
+      break;
+    default:
+      break;
   }
-  console.log(`Human Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
+});
 
-  if (humanScore === computerScore) console.log("A Draw!");
-  else if (humanScore < computerScore) console.log("You Lost!!");
-  else console.log("YOU WON!!!!");
+const humanScr = document.querySelector(".human-scr");
+const computerScr = document.querySelector(".computer-scr");
+const display = document.querySelector("#display");
+
+function playGame(s) {
+  playRound(s, getComputerChoice());
+  humanScr.textContent = `Human Score: ${humanScore}`;
+  computerScr.textContent = `Computer Score: ${computerScore}`;
+
+  if (humanScore == 5 || computerScore == 5) {
+    display.style.fontSize = "48px";
+    display.style.textAlign = "center";
+    display.style.fontWeight = "800";
+    if (humanScore === computerScore) {
+      display.textContent = "A Draw!";
+    } else if (humanScore < computerScore) {
+      display.textContent = "You Lost!!";
+    } else {
+      display.textContent = "YOU WON!!!!";
+    }
+  }
 }
-
-playGame();
